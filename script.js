d241e67044ec7121d7287b882145b6652d036ce8
Marcus <marcusviniciusjp@hotmail.com>
2024-02-12 22:38:37 -0300

    Function + operador booleano.

--- script.js
+++ script.js
@@ -55,3 +55,46 @@
 
 // window.alert("flashback");
 
+
+
+// ------------------11/02/2024------------------------
+// let x = 100;
+
+// x = 200;
+// console.log(x);
+
+// function sayMyName(nome){
+//     console.log('My name is ' + nome)
+// }
+
+// sayMyName('maria');
+
+// function somar(x, y){
+//     return x + y
+// }
+
+// console.log(somar(10, 20));
+
+// let age = 30;
+
+// let city = "Lisboa";
+
+// let money = 40;
+
+// console.log(`You are ${age} years old!`);
+// console.log(`You live in ${city}!`);
+// console.log(`You have ${money} dollars`);
+
+// let nome = false;
+
+// console.log(`Marcus is online: ${nome}`);
+
+// let FullName = "Marcus Vinícius";
+
+// let City = "João Pessoa";
+
+// let car = true;
+
+// document.getElementById("p1").textContent = `My name is ${FullName}`;
+// document.getElementById("p2").textContent = `I live in ${City}`;
+// document.getElementById("p3").textContent = `I have a car: ${car} `;
