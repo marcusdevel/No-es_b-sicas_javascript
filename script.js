ad1b9baaa3ea1500b5e4a834e0dfdba7aceb84cd
Marcus <marcusviniciusjp@hotmail.com>
2024-02-12 22:35:02 -0300

    Noções básicas de Js + variáveis

---
+++ script.js
@@ -0,0 +1,57 @@
+// -------------08/02/2024------------------------
+
+// console.log("Hello World");
+
+// window.alert('This is an alert');
+
+// NOÇÕES BÁSICAS DE JS --------------------------------
+// window.alert("OKOK");
+
+// document.getElementById("myh1").textContent = "Hello World";
+
+// document.getElementById("myP").textContent = "Hi, Maria";
+
+// document.getElementById("myh1").textContent = "hi, tom";
+
+// document.getElementById("myh1").style.color = "red";
+
+// document.getElementById("myP").style.backgroundColor = "blue";
+
+// document.getElementById("myh1").style.display = "inline-block"
+
+// document.getElementById("myP").style.display = "block"
+
+// document.getElementById("myh1").style.backgroundColor = "yellow";
+
+// document.getElementById("myh1").style.textAlign = "right";
+
+// ------------------------------------------------------------
+
+// let x=100;  (DECLARAÇÃO DE VARIAVEL)
+
+// let x;
+
+// x = 200; (ATRIBUIÇÃO DE  VALOR)
+// let age = 30;
+// let value = 40.99;
+// let nome = "maria";
+// let city = "João Pessoa";
+
+// console.log(`You are ${age} years old!`);
+// console.log(`You have $${value} dollars!`);
+// console.log(`Her name is ${nome} and she lives in ${city}`);
+// console.log(typeof age);
+// console.log(typeof value);
+// console.log(typeof nome);
+// console.log(typeof city);
+
+// document.getElementById("myh1").style.color = "blue";
+
+// document.getElementById("myP").style.backgroundColor = "green";
+
+// document.getElementById("myh1").textContent = "VARUS";
+
+// document.getElementById("myh1").style.backgroundColor = "pink";
+
+// window.alert("flashback");
+
